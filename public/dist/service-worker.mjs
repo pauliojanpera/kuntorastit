677cb04ce0bb19435f85da21e2419411fd9d1555
Pauli Ojanperä <pauli.ojanpera@miettii.net>
2025-02-18 16:40:55 +0200

    correct cache list again

--- public/dist/service-worker.mjs
+++ public/dist/service-worker.mjs
@@ -1,5 +1,10 @@
 const CACHE_NAME = "kuntorastit";
-const URLS_TO_CACHE = ["/", "/index.html", "/dist/app.mjs", "/data/events.json"];
+const URLS_TO_CACHE = [
+    "index.html",
+    "dist/kuntorastit.mjs",
+    "data/events.json",
+    "icon-192.png",
+];
 self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
 });
