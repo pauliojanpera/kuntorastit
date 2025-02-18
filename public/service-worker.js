50f76ade022798325bfc98b3f7a0214c2513ed37
Pauli Ojanperä <pauli.ojanpera@miettii.net>
2025-02-18 16:07:05 +0200

    service-worker.js symlink

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,1 @@
-const CACHE_NAME = "kuntorastit";
-const URLS_TO_CACHE = ["/", "/index.html", "/dist/app.mjs", "/data/events.json"];
-self.addEventListener("install", (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
-});
-self.addEventListener("fetch", (event) => {
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
-export {};
+dist/service-worker.mjs
